b3624f5ec20afc0df254dfb08cdb72ac850b7862
whoisoscar <tluszczoscar@gmail.com>
2022-12-08 17:26:49 +0100

    reduced redundant if statements, made algo more efficient

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 import random
-from heapq import heappop, heappush, heapify
-#heapq_max
+#from heapq import heappop, heappush, heapify
+from heapq_max import heappop_max, heappush_max, heapify_max
+
 import csv
 
 app = Flask(__name__)
@@ -16,31 +17,12 @@ def questionaire():
     data = request.form
 
     def convert_questionnaire():
-        if data.get("q1") == "No":
+        if data.get("q1") == "No" or data.get("q3") == "N/A" or data.get("q4") == "N/A" or data.get("q5") == "N/A":
+            #In this case, education is not considered
             user_education = 0
 
-            if data.get("q8") == "Yes":
-                user_safety = 5
-            elif data.get("q8") == "No":
-                user_safety = 2
-            
-    
-            if data.get("q9") == "Yes":
-                user_safety += 2
-            elif data.get("9") == "No":
-                user_safety += 4
-    
-            user_safety += int(data.get("q6"))
-
-            user_safety += int(data.get("q7"))
-
-            user_safety += int(data.get("q10"))
-
-            user_safety = int(round(user_safety/25*5,0))
-
-            return user_safety, user_education
-
         else:
+            #In this case, education is considered and calculated
             if data.get("q2") == "Yes":
                 user_education = 8
             elif data.get("q2") == "No":
@@ -60,30 +42,25 @@ def questionaire():
             user_education = user_education + (int(data.get("q5")) + int(data.get("q5")) - 1)
 
             user_education = int(round(user_education/34*9,0))
-            
-            if data.get("q8") == "Yes":
-                user_safety = 5
-            elif data.get("q8") == "No":
-                user_safety = 2
-    
-            if data.get("q9") == "Yes":
-                user_safety += 2
-            elif data.get("q9") == "No":
-                user_safety += 4
-            
-            user_safety += int(data.get("q6"))
+        
+        #Calculating safety
+        user_safety = 5 if data.get("q8") == "Yes" else 2
 
-            user_safety += int(data.get("q7"))
+        user_safety += 2 if data.get("q9") == "Yes" else 4
 
-            user_safety += int(data.get("q10"))
+        user_safety += int(data.get("q6"))
 
-            user_safety = int(round(user_safety/25*5,0))
+        user_safety += int(data.get("q7"))
+
+        user_safety += int(data.get("q10"))
+
+        user_safety = int(round(user_safety/25*5,0))
 
         return user_safety, user_education
     
 
     safety, education = convert_questionnaire()
-    print(safety, education)
+
     letter_convert = {
         9:"A%2B",
         8:"A",
@@ -108,7 +85,7 @@ def search_results():
     state = request.args.get("state")
     education = request.args.get("education")
     safety = request.args.get("safety")
-    print(safety)
+
     price_range = request.args.get("price_range")
 
     letter_convert = {
@@ -144,7 +121,7 @@ def search_results():
 
     #Creating empty binary tree
     possible_neighbours = []
-    heapify(possible_neighbours)
+    heapify_max(possible_neighbours)
 
     #Creating empty final list
     display = []
@@ -159,82 +136,38 @@ def search_results():
             self.price = int(price.replace(",", ""))
             self.rank = int(0)
 
-        def points(self, input_education, input_safety, input_price):
+        def calculate_points(self, input_education, input_safety, input_price):
             if self.price > input_price:
                 return None
 
             else:
-                if self.education == input_education:
-                    self.rank += 2
+                difference_to_rank = {
+                        0: 2,
+                        -1:1,
+                        1:3,
+                        2:4,
+                        3:4,
+                        4:4,
+                        5:4,
+                        6:4,
+                        -2:0,
+                        -3:-1,
+                        -4:-1,
+                        -5:-2,
+                        -6:-2
+                    }
+                if input_education != 0:
+                    #If education level is 0, skip this part
+                    
+                    #Difference between actual education & input
+                    difference = self.education - input_education
+                    self.rank += difference_to_rank[difference]
 
-                if self.education == (input_education - 1):
-                    self.rank += 1
-
-                if self.education == (input_education + 1):
-                    self.rank += 3
-
-                if self.education == (input_education + 2) or self.education == (input_education + 3) or self.education == (input_education + 4) or self.education == (input_education + 5) or self.education == (input_education + 6):
-                    self.rank += 4
-
-                if self.education == (input_education - 2):
-                    self.rank = self.rank
-
-                if self.education == (input_education - 3) or self.education == (input_education - 4):
-                    self.rank -= 1
-
-                if self.education == (input_education - 5) or self.education == (input_education - 6):
-                    self.rank -= 2
-
-                if self.safety == input_safety:
-                    self.rank += 2
-
-                if self.safety == (input_safety - 1):
-                    self.rank += 1
-
-                if self.safety == (input_safety + 1):
-                    self.rank += 3
-
-                if self.safety == (input_safety + 2) or self.safety == (input_safety + 3) or self.safety == (input_safety + 4) or self.safety == (input_safety + 5) or self.safety == (input_safety + 6):
-                    self.rank += 4
-
-                if self.safety == (input_safety - 2):
-                    self.rank = self.rank
-
-                if self.safety == (input_safety - 3) or self.safety == (input_safety - 4):
-                    self.rank -= 1
-
-                if self.safety == (input_safety - 5) or self.safety == (input_safety - 6):
-                    self.rank -= 2
-
-                heappush(possible_neighbours, (-1*self.rank, self.name))
-
-        def no_education_points(self, input_safety, input_price):
-            if self.price > input_price:
-                return None
-
-            else:
-                if self.safety == input_safety:
-                    self.rank += 2
-
-                if self.safety == (input_safety - 1):
-                    self.rank += 1
-
-                if self.safety == (input_safety + 1):
-                    self.rank += 3
-
-                if self.safety == (input_safety + 2) or self.safety == (input_safety + 3) or self.safety == (input_safety + 4) or self.safety == (input_safety + 5) or self.safety == (input_safety + 6):
-                    self.rank += 4
-
-                if self.safety == (input_safety - 2):
-                    self.rank = self.rank
-
-                if self.safety == (input_safety - 3) or self.safety == (input_safety - 4):
-                    self.rank -= 1
-
-                if self.safety == (input_safety - 5) or self.safety == (input_safety - 6):
-                    self.rank -= 2
-
-                heappush(possible_neighbours, (-1*self.rank, self.name))
+                #Difference between actual safety & input
+                difference = self.safety - input_safety
+                self.rank += difference_to_rank[difference]
+                
+                heappush_max(possible_neighbours, (self.rank, self.name))
 
 
     LA_database = []
@@ -246,50 +179,34 @@ def search_results():
 
     # Creating function to use method of distric and rank them
     def ranking(database, user_education, user_safety, user_price):
-        if user_education == 0:
-            for i in database:
-                i.no_education_points(user_safety, user_price)
-
-            while len(display) != 5:
-                aux = heappop(possible_neighbours)
-                for i in LA_database:
-                    if i.name == aux[1]:
-                        aux2 = [aux[0], aux[1], i.price, i.safety, i.education]
-
-                display.append(aux2)
-                display.sort(key=lambda e: (e[0], e[2]))
+        
+        for i in database:
             
-            return display
+            i.calculate_points(user_education, user_safety, user_price)
 
-        else:
-            for i in database:
-                i.points(user_education, user_safety, user_price)
+        while len(display) != 5:
+            try:
+                aux = heappop_max(possible_neighbours)
+            except:
+                return "No results found"
+            
+            for i in LA_database:
+                if i.name == aux[1]:
+                    aux2 = [aux[0], aux[1], i.price, i.safety, i.education]
 
-            while len(display) != 5:
-                try:
-                    aux = heappop(possible_neighbours)
-                except:
-                    return "No results found"
-                for i in LA_database:
-                    if i.name == aux[1]:
-                        aux2 = [aux[0], aux[1], i.price, i.safety, i.education]
-
-                display.append(aux2)
-                display.sort(key=lambda e: (e[0], e[2]))
-
-            return display
-
+            display.append(aux2)
+            display.sort(key=lambda e: (e[0], e[2]))
+        
+        return display
 
     results = ranking(LA_database, backend_education, backend_safety, backend_price_range[0])
     if results == "No results found":
         return "No results found"
-    print(results)
+
     for result in results:
-        print("AA",result)
         result[2] = f'{result[2]:,}'
         result[3] = letter_convert_back[result[3]]
         result[4] = letter_convert_back[result[4]]
     
-    print(results)
 
     return render_template('results.html', state=state, education=education, safety=safety, price_range=price_range, random_num=random.randint(9,60), results=results)
